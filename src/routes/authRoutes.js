521fcdd048697b2a09ef71b06c5b26e04dd96933
Carlos Baez <c.baez1223@yahoo.com>
2021-02-11 12:25:32 -0500

    generated a jwt for new users

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const jst = require('jsonwebtoken')
 const User = mongoose.model('User')
 
 const router = express.Router()
@@ -10,9 +11,10 @@ router.post('/signup', async (req, res) => {
     try {
         const user = new User({ email, password })
         await user.save() 
-    
+        
+        const token = jst.sign({ userId: user._id }, 'MY_SECRET_KEY')
         console.log(req.body)
-        res.send('You made a post request!!!')
+        res.send({ message: 'Congratulations for signing up!', token })
 
     } catch (err) {
         return res.status(422).send(err.message)
