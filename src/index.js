72360addac09a32c47ae32744560f37d3e32e991
Carlos Baez <c.baez1223@yahoo.com>
2021-02-10 22:55:14 -0500

    inital commit and connect express to mongoDB

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
+
+const app = express()
+
+app.use(authRoutes)
+
+const mongoUri = 'mongodb+srv://admin:admin12345@cluster0.dxng7.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo! 2021')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
