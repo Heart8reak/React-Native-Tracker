54461c9f85e9660ab250faa7ab3421c9117a5d65
Carlos Baez <c.baez1223@yahoo.com>
2021-02-11 21:05:33 -0500

    created a middleware to extract the jwt for the users

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
+const requireAuth = require('./middlewares/requireAuth')
 
 const app = express()
 
@@ -23,8 +24,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err)
 })
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo! 2021')
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`)
 })
 
 app.listen(3000, () => {
